1084a7c2c68e6a8b9353b3eca453093aefa982c6
Ivan <ivanpercuk321@gmail.com>
2024-06-02 18:57:55 +0200

    m

---
+++ WebApplication/Models/AddToStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class AddToStore
+    {
+        public int StoreID { get; set; }
+        public int AlbumID { get; set; }
+
+        public List<Album> Albums { get; set; }
+
+        public AddToStore()
+        {
+            Albums = new List<Album>();
+        }
+    }
+}
---
+++ WebApplication/Models/UserRoles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class UserRoles
+    {
+        public string email {  get; set; }
+        public string selectedRole { get; set; }
+
+        public List<string> Roles { get; set; }
+
+        public UserRoles() {
+            Roles = new List<string>()
+            {
+                "Editor",
+                "Admin",
+                "User"
+            };        
+        }
+    }
+}
